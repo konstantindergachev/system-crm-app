72497414f70ba702fcee01f670b4ffe380fb12d4
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-08-30 09:37:29 +0300

    Added some setting props

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,8 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
+  imageUrl: process.env.IMAGE_URL,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRETE,
 };
